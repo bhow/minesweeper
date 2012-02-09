f313c18acc4f6ad1cf1510a509285f1aab4216ce
Brian How <bkhow@yahoo.com>
2012-02-09 03:20:18 -0800

    basic board setup and tile uncovering working

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,35 @@
+Array.prototype.contains = function(obj) {
+  for (i in this) {
+    if (this[i] === obj) return true;
+  }
+  return false;
+}
+
 Minesweeper = Ember.Application.create();
 
+// models
 Minesweeper.Board = Em.Object.extend({
-  tileArray : null,
+  tileArray: null,
   size: 0,
-  
-  // initialize board and tiles
-  reset: function(size) {
+  bombTileArray: null,  
+
+  // initialize board
+  reset: function(size, bombCount) {
+    
+    // generate bomb positions
+    var bombIndices = new Array();
+    
+    var totalTiles = size * size;
+    while (bombIndices.length < bombCount) {
+      var randomIndex = (Math.floor(Math.random() * totalTiles));
+      do {
+        if (!bombIndices.contains(randomIndex)) { 
+          bombIndices.push(randomIndex);
+        }
+        randomIndex = (Math.floor(Math.random() * totalTiles));
+      } while (bombIndices.length < bombCount)
+    }
+    console.log(bombIndices);
     this.set('size', size);
     this.set('tileArray', new Array(size));
     
@@ -13,7 +37,41 @@ Minesweeper.Board = Em.Object.extend({
       this.tileArray[i] = new Array(size);
       for (var j=0; j<size; j++) {
         var tile = Minesweeper.Tile.create();
-        tile.addToBoard(i, j, this);
+        var position = (i * size) + j;
+        if (bombIndices.contains(position)) {
+          tile.set('containsBomb', true);
+        }
+        this.addTileToBoard(i, j, tile);
+      }
+    }
+  },
+  
+  addTileToBoard: function(row, column, tile) {
+    tile.set('row', row);
+    tile.set('column', column);
+    this.get('tileArray')[row][column] = tile;
+  },
+
+  revealTile: function(tile) {
+    if (!tile.get('hidden')) {
+      return;
+    }
+    tile.set('hidden', false);
+    if (tile.get('containsBomb')) {
+      // boom - end game here
+    }
+    var surroundingTiles = this.getSurroundingTiles(tile);
+    var surroundingBombs = 0;
+    for (var i=0; i < surroundingTiles.length; i++) {
+      if (surroundingTiles[i].get('containsBomb')) {
+        surroundingBombs += 1;
+      }
+    }
+    tile.set('bombTouchCount', surroundingBombs);
+    if (surroundingBombs === 0) {
+      //if there aren't any bombs we reveal everything around this tile
+      for (var i=0; i < surroundingTiles.length; i++) {
+        this.revealTile(surroundingTiles[i]);
       }
     }
   },
@@ -53,75 +111,73 @@ Minesweeper.Board = Em.Object.extend({
     if (!onRightEdge) {
       surroundingTiles.push(this.get('tileArray')[row][column+1]);
     }
-  }
-});
-
-Minesweeper.GameManager = Em.Object.extend({
-  board: Minesweeper.Board.create(),
-  startGame: function() {
-    console.log("initing gamemanager");
-    this.board.reset(16);
+    return surroundingTiles;
   }
 });
 
 Minesweeper.Tile = Em.Object.extend({
-  covered: true, // content of this tile has not been exposed
+  hidden: true, // content of this tile has not been exposed
   flagged: false, // user has flagged this as a bomb
   containsBomb: false,
   bombTouchCount: 0,
   row: 0,
   column: 0,
   board: null,
-  
-  addToBoard: function(row, column, board) {
-    this.set('row', row);
-    this.set('column', column);
-    this.set('board', board);
-    board.get('tileArray')[row][column] = this;
-  },
-
-  reveal: function() {
-    if (containsBomb) {
-      // boom - end game here
-    }
-    var surroundingTiles = this.get('board').getSurroundingTiles(this);
-    var surroundingBombs = 0;
-    for (var i=0; i < surroundingTiles.length; i++) {
-      if (surroundingTiles[i].get('containsBomb')) {
-        surroundingBombs += 1;
-      }
-    }
-    this.set('bombTouchCount', surroundingBombs);
-    if (surroundingBombs === 0) {
-      //if there aren't any bombs we reveal everything around this tile
-      for (var i=0; i < surroundingTiles.length; i++) {
-        surroundingTiles[i].reveal();
-      }
-    }
-  },
   flag: function() {
-    if (this.covered) {
-      this.set('flagged', true); 
+    if (this.hidden) {
+      this.set('flagged', !this.get('flagged')); 
     }
+  },
+  style: function() {
+    if (this.get('hidden')) {
+      if (this.get('flagged')) {
+        return "flagged";
+      } else {
+        return "hidden";
+      }
+    }
+    if (this.get('containsBomb')) {
+      console.log(this.get('containsBomb'));
+      return "bomb";
+    }
+    if (this.get('bombTouchCount') === 0) {
+      return "empty";
+    }
+    var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight"];
+    return numbers[this.get('bombTouchCount')];
+  }.property('hidden', 'flagged', 'containsBomb', 'bombTouchCount')
+});
+
+// controllers
+Minesweeper.minesweeperController = Ember.Object.create({
+  board: Minesweeper.Board.create(),
+  start: function() {
+    this.board.reset(16, 32);
+  },
+  clickTile: function(tile) {
+    this.board.revealTile(tile);
   }
 });
 
-
-var gameManager = Minesweeper.GameManager.create();
-gameManager.startGame();
-console.log(gameManager.get('board'));
+Minesweeper.minesweeperController.start();
 
 // views
-Minesweeper.SingleTileView = Ember.View.create({
-  templateName: 'single-tile-view',
-  click: function(evt) {
-    alert("ClickableView was clicked!");
-  }
-});
+Minesweeper.StatView = Ember.View.extend({
+}
 
 Minesweeper.BoardView = Ember.View.create({
   templateName: 'board-view',
-  board: gameManager.get('board')
+  tileArray: Minesweeper.minesweeperController.board.get('tileArray'),
+});
+
+Minesweeper.TileView = Ember.View.extend({
+  templateName: 'tile-view',
+  tile: null,
+  tileStyleBinding: 'this.tile.style',
+  tileCommonStyle: 'tile',
+  click: function(evt) {
+    Minesweeper.minesweeperController.clickTile(this.tile);
+  },
 });
 
 Minesweeper.BoardView.append();
